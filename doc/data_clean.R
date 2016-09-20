ab35aab3dd32e9ff777092329c2d727565eb0d57
Jiayiwang7 <jiayiwang7@yahoo.com>
2016-09-20 01:21:38 -0400

    data cleaning

---
+++ doc/data_clean.R
@@ -0,0 +1,40 @@
+library(data.table)
+
+data1 <- fread('ss14pusa.csv')
+data2 <- fread('ss14pusb.csv')
+data_ori <- rbind(data1, data2)
+as.data.table(data_ori)
+
+databad <- data_ori[(MAR == 3 | MARHT >= 2)]
+datagood <- data_ori[(MAR != 3 & MARHT < 2)]
+datagood[, fac := 1, ]
+databad1 <- databad[!(MARHT >= 3 | (MARHT == 2 & MAR == 3))]
+databad1[, fac := 2, ]
+databad2 <- databad[MARHT >= 3 | (MARHT == 2 & MAR == 3)]
+databad2[, fac := 3, ]
+data_f <- rbind(datagood, databad1, databad2)
+
+
+data_final <- data_f
+data_dis <- data_final[!is.na(DIS), list(DIS, fac)]
+write.table(data_dis, 'data_dis.csv',sep = ',', row.names = F)
+
+data_final <- data_f
+data_RAC1P <- data_final[!is.na(RAC1P), list(RAC1P, fac)]
+write.table(data_RAC1P, 'data_RAC1P.csv', sep = ',', row.names = F)
+
+data_final <- data_f
+data_SCHL <- data_final[!is.na(SCHL), list(SCHL, fac)]
+write.table(data_SCHL, 'data_SCHL.csv', sep = ',', row.names = F)
+
+data_final <- data_f
+data_WAGP <- data_final[!is.na(WAGP), list(WAGP, fac)]
+write.table(data_WAGP, 'data_WAGP.csv', sep = ',', row.names = F)
+
+data_final <- data_f
+data_VPS <- data_final[!is.na(VPS), list(VPS, fac)]
+write.table(data_VPS, 'data_VPS.csv', sep = ',', row.names = F)
+
+
+
+
