4ca0080f787989aac90d1ea99ec27204a0d806ec
Xuechun (Alexa) Sun <1181509338@qq.com>
2016-09-20 02:45:15 -0400

    Update data_clean.R

--- doc/data_clean.R
+++ doc/data_clean.R
@@ -30,12 +30,17 @@ write.table(data_dis, 'data_dis.csv',sep = ',', row.names = F)
 # 3 .American Indian alone
 # 6 .Asian alone
 data_final <- data_f
-data_RAC1P <- data_final[!is.na(RAC1P), list(RAC1P, fac)]
+data_RAC1P <- data_final[RAC1P==1|RAC1P==2|RAC1P==3|RAC1P==6, list(RAC1P, fac)]
 write.table(data_RAC1P, 'data_RAC1P.csv', sep = ',', row.names = F)
 
 #SCHL (Educational attainment)
+# 1 .No schooling completed
+# 16 .Regular high school diploma
+# 21 .Bachelor's degree
+# 22 .Master's degree
+# 24 .Doctorate degree
 data_final <- data_f
-data_SCHL <- data_final[!is.na(SCHL), list(SCHL, fac)]
+data_SCHL <- data_final[SCHL==1|SCHL==16|SCHL==21|SCHL==22|SCHL==24, list(SCHL, fac)]
 write.table(data_SCHL, 'data_SCHL.csv', sep = ',', row.names = F)
 
 #WAGP (Wages or salary income past 12 months)
@@ -44,9 +49,21 @@ data_WAGP <- data_final[!is.na(WAGP), list(WAGP, fac)]
 write.table(data_WAGP, 'data_WAGP.csv', sep = ',', row.names = F)
 
 #VPS (Veteran period of service)
+# 1 .Gulf War: 9/2001 or later
+# 6 .Vietnam Era
+# 11 .WWII
 data_final <- data_f
-data_VPS <- data_final[!is.na(VPS), list(VPS, fac)]
+data_VPS <- data_final[VPS==1|VPS==6|VPS==11, list(VPS, fac)]
 write.table(data_VPS, 'data_VPS.csv', sep = ',', row.names = F)
+
+#MIL (Military service)
+# 1 .Now on active duty
+# 2 .On active duty in the past, but not now
+# 3 .Only on active duty for training in Reserves/National Guard 
+# 4 .Never served in the military
+data_final <- data_f
+data_MIL <- data_final[!is.na(MIL), list(MIL, fac)]
+write.table(data_MIL, 'data_MIL.csv', sep = ',', row.names = F)
 
 
 
