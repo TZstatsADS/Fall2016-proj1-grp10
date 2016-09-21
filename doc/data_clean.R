081b8699f16a0dad63884dbcfe8badb8401205a9
Xuechun (Alexa) Sun <1181509338@qq.com>
2016-09-20 22:06:09 -0400

    Update data_clean.R

--- doc/data_clean.R
+++ doc/data_clean.R
@@ -77,10 +77,7 @@ write.table(data_MIL, 'data_MIL.csv', sep = ',', row.names = F)
 
 # Data contains all vars mentioned above
 data_final <- data_f
-data_all_var <- data_final[!is.na(DIS) & (RAC1P==1|RAC1P==2|RAC1P==3|RAC1P==6) & (SCHL==1|SCHL==16|SCHL==21|SCHL==22|SCHL==24) & !is.na(WAGP) & (VPS==1|VPS==6|VPS==11) & !is.na(MIL), list(DIS, RAC1P, SCHL, WAGP, VPS, MIL, fac)]
+data_all_var <- data_final[!is.na(DIS) & (RAC1P==1|RAC1P==2|RAC1P==3|RAC1P==6) & (SCHL==1|SCHL==16|SCHL==21|SCHL==22|SCHL==24) & !is.na(WAGP) & (VPS==1|VPS==6|VPS==11) & !is.na(MIL), list(DIS, RAC1P, SCHL, WAGP, VPS, MIL, PWGTP, fac)]
 write.table(data_all_var, 'data_all_var.csv', sep = ',', row.names = F)
-
-
-
 
 
