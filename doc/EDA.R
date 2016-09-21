4283326027dc4e1723553fd4508b7b2f1cf405cc
Xuechun (Alexa) Sun <xuechunsun@gmail.com>
2016-09-21 12:32:23 -0400

    dependency plot

--- doc/EDA.R
+++ doc/EDA.R
@@ -1,5 +1,6 @@
 library(data.table)
 library(vcd)
+library(dplyr)
 #set direction
 getwd()
 setwd("/Users/sun93/Documents/ADS/Fall2016-proj1-grp10/")
@@ -15,55 +16,142 @@ schl <- fread('data/data_SCHL.csv')
 #load veteran file
 vps <- fread('data/data_vps.csv') 
 
+#############################################################
 # mosaic plot based on disability
 dis <- as.data.frame(dis)
-disibility <- factor(dis[,1],labels=c("with dis","without dis"))
-marriage.status <- factor(dis[,2],labels = c("good","not good","bad"))
+#head(dis)
+# add weight to count
+dis.num<- count(dis, DIS, wt = PWGTP)
+dis1fac.num <- count(dis[which(dis$DIS==1),], fac, wt = PWGTP)
+dis2fac.num <- count(dis[which(dis$DIS==2),], fac, wt = PWGTP)
+#subdata = data[sample(1:dim(data)[1], as.integer(dim(data)[1]/10), replace=FALSE),]
 
-mosaic(~marriage.status+disibility,data = dis,shade=TRUE,labeling=labeling_values,
+#set up dataframe
+disibility <- factor(rep(1:2,c(dis.num$n)),labels=c("with dis","without dis"))
+marriage.status <- factor(c(rep(1:3,c(dis1fac.num$n)),rep(1:3,c(dis2fac.num$n))),labels = c("good","not good","bad"))
+dis.plot <- data.frame(disibility,marriage.status)
+#get 10% sample
+subdis.plot <- dis.plot[sample(1:dim(dis.plot)[1], as.integer(dim(dis.plot)[1]/10), replace=FALSE),]
+
+#mosic plot
+mosaic(~marriage.status+disibility,data = subdis.plot,shade=TRUE,labeling=labeling_values,
        main = "Dependency Plot about Disibility and Marraige Status", 
        sub = "Red: less likely   Blue: more likely")
 
+#disibility <- factor(dis[,1],labels=c("with dis","without dis"))
+#marriage.status <- factor(dis[,3],labels = c("good","not good","bad"))
 
+
+#############################################################
 # mosaic plot based on military
 mil <- as.data.frame(mil)
-military <- factor(mil[,1],labels=c("Now","Past","only","Never"))
-marriage.status <- factor(mil[,2],labels = c("good","not good","bad"))
+#head(mil)
 
-mosaic(~marriage.status+military,data = mil,shade=TRUE,labeling=labeling_values,
+# add weight to count
+mil.num<- count(mil, MIL, wt = PWGTP)
+mil1fac.num <- count(mil[which(mil$MIL==1),], fac, wt = PWGTP)
+mil2fac.num <- count(mil[which(mil$MIL==2),], fac, wt = PWGTP)
+mil3fac.num <- count(mil[which(mil$MIL==3),], fac, wt = PWGTP)
+mil4fac.num <- count(mil[which(mil$MIL==4),], fac, wt = PWGTP)
+#set up dataframe
+military <- factor(rep(1:4,c(mil.num$n)),labels=c("Now","Past","only","Never"))
+marriage.status <- factor(c(rep(1:3,c(mil1fac.num$n)),rep(1:3,c(mil2fac.num$n)),rep(1:3,c(mil3fac.num$n)),rep(1:3,c(mil4fac.num$n))),labels = c("good","not good","bad"))
+mil.plot <- data.frame(military,marriage.status)
+#get 10% sample
+submil.plot <- mil.plot[sample(1:dim(mil.plot)[1], as.integer(dim(mil.plot)[1]/10), replace=FALSE),]
+
+#mosaic plot
+mosaic(~marriage.status+military,data = submil.plot,shade=TRUE,labeling=labeling_values,
        main = "Dependency Plot about Military and Marraige Status", 
        sub = "Red: less likely   Blue: more likely")
 
+#military <- factor(mil[,1],labels=c("Now","Past","only","Never"))
+#marriage.status <- factor(mil[,3],labels = c("good","not good","bad"))
 
-
+#############################################################
 # mosaic plot based on race
 rac <- as.data.frame(rac)
-race <- factor(rac[,1],labels=c("White","Black","Indian","Asian"))
-marriage.status <- factor(rac[,2],labels = c("good","not good","bad"))
+#head(rac)
 
-mosaic(~marriage.status+race,data = rac,shade=TRUE,labeling=labeling_values,
+# add weight to count
+rac.num<- count(rac, RAC1P, wt = PWGTP)
+rac1fac.num <- count(rac[which(rac$RAC1P==1),], fac, wt = PWGTP)
+rac2fac.num <- count(rac[which(rac$RAC1P==2),], fac, wt = PWGTP)
+rac3fac.num <- count(rac[which(rac$RAC1P==3),], fac, wt = PWGTP)
+rac4fac.num <- count(rac[which(rac$RAC1P==6),], fac, wt = PWGTP)
+#set up dataframe
+race <- factor(rep(1:4,c(rac.num$n)),labels=c("White","Black","Indian","Asian"))
+marriage.status <- factor(c(rep(1:3,c(rac1fac.num$n)),rep(1:3,c(rac2fac.num$n)),rep(1:3,c(rac3fac.num$n)),rep(1:3,c(rac4fac.num$n))),labels = c("good","not good","bad"))
+rac.plot <- data.frame(race,marriage.status)
+#get 10% sample
+subrac.plot <- rac.plot[sample(1:dim(rac.plot)[1], as.integer(dim(rac.plot)[1]/10), replace=FALSE),]
+
+#mosaic plot
+mosaic(~marriage.status+race,data = subrac.plot,shade=TRUE,labeling=labeling_values,
        main = "Dependency Plot about Race and Marraige Status", 
        sub = "Red: less likely   Blue: more likely")
 
+#race <- factor(rac[,1],labels=c("White","Black","Indian","Asian"))
+#marriage.status <- factor(rac[,2],labels = c("good","not good","bad"))
 
-# mosaic plot based on race
+
+#############################################################
+# mosaic plot based on education
 schl <- as.data.frame(schl)
-education <- factor(schl[,1],labels=c("No","High schl","Bachelor","Master","Doctorate"))
-marriage.status <- factor(schl[,2],labels = c("good","not good","bad"))
 
-mosaic(~marriage.status+education,data = schl,shade=TRUE,labeling=labeling_values,
+#head(schl)
+
+# add weight to count
+schl.num<- count(schl, SCHL, wt = PWGTP)
+schl1fac.num <- count(schl[which(schl$SCHL==1),], fac, wt = PWGTP)
+schl16fac.num <- count(schl[which(schl$SCHL==16),], fac, wt = PWGTP)
+schl21fac.num <- count(schl[which(schl$SCHL==21),], fac, wt = PWGTP)
+schl22fac.num <- count(schl[which(schl$SCHL==22),], fac, wt = PWGTP)
+schl24fac.num <- count(schl[which(schl$SCHL==24),], fac, wt = PWGTP)
+#set up dataframe
+education <- factor(rep(1:5,c(schl.num$n)),labels=c("No","High schl","Bachelor","Master","Doctorate"))
+marriage.status <- factor(c(rep(1:3,c(schl1fac.num$n)),rep(1:3,c(schl16fac.num$n)),rep(1:3,c(schl21fac.num$n)),rep(1:3,c(schl22fac.num$n)),rep(1:3,c(schl24fac.num$n))),labels = c("good","not good","bad"))
+schl.plot <- data.frame(education,marriage.status)
+#schl.plot$
+#get 10% sample
+subschl.plot <- schl.plot[sample(1:dim(schl.plot)[1], as.integer(dim(schl.plot)[1]/10), replace=FALSE),]
+#subschl.plot$
+mosaic(~marriage.status+education, data = subschl.plot,shade=TRUE,labeling=labeling_values,
        main = "Dependency Plot about Education and Marraige Status", 
        sub = "Red: less likely   Blue: more likely")
 
 
+#education <- factor(schl[,1],labels=c("No","High schl","Bachelor","Master","Doctorate"))
+#marriage.status <- factor(schl[,2],labels = c("good","not good","bad"))
+
+
+#############################################################
 # mosaic plot based on veteran
 vps <- as.data.frame(vps)
-veteran <- factor(vps[,1],labels=c("Gulf War","Vietnam Era","WWII"))
-marriage.status <- factor(vps[,2],labels = c("good","not good","bad"))
+#head(vps)
 
-mosaic(~marriage.status+veteran,data = vps,shade=TRUE,labeling=labeling_values,
+# add weight to count
+vps.num<- count(vps, VPS, wt = PWGTP)
+vps1fac.num <- count(vps[which(vps$VPS==1),], fac, wt = PWGTP)
+vps6fac.num <- count(vps[which(vps$VPS==6),], fac, wt = PWGTP)
+vps11fac.num <- count(vps[which(vps$VPS==11),], fac, wt = PWGTP)
+
+#set up dataframe
+veteran <- factor(rep(1:3,c(vps.num$n)),labels=c("Gulf War","Vietnam Era","WWII"))
+marriage.status <- factor(c(rep(1:3,c(vps1fac.num$n)),rep(1:3,c(vps6fac.num$n)),rep(1:3,c(vps11fac.num$n))),labels = c("good","not good","bad"))
+vps.plot <- data.frame(veteran,marriage.status)
+#get 10% sample
+subvps.plot <- vps.plot[sample(1:dim(vps.plot)[1], as.integer(dim(vps.plot)[1]/10), replace=FALSE),]
+
+mosaic(~marriage.status+veteran,data = subvps.plot,shade=TRUE,labeling=labeling_values,
        main = "Dependency Plot about Veteran and Marraige Status", 
        sub = "Red: less likely   Blue: more likely")
+
+
+#veteran <- factor(vps[,1],labels=c("Gulf War","Vietnam Era","WWII"))
+#marriage.status <- factor(vps[,2],labels = c("good","not good","bad"))
+
+
 
 
 
